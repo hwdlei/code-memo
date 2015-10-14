f86d9a489ec1c7285c80faf2962bbd72fab01240
donglei <donglei@zxisl.com>
2015-10-14 20:26:54 +0800

    修改kafka producer

--- kafka-demo/src/main/java/edu/hfut/kafka/producer/ProducerExample.java
+++ kafka-demo/src/main/java/edu/hfut/kafka/producer/ProducerExample.java
@@ -7,17 +7,14 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-/**
- * An example using the new java client Producer for Kafka 0.8.2
- *
- * @author Cameron Gregory, http://www.bloke.com/
- */
-public class TestProducer {
+public class ProducerExample {
 	public static void main(String args[]) throws InterruptedException, ExecutionException {
 		Properties props = new Properties();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		//		props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
 		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
